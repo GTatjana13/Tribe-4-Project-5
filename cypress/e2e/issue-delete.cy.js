99fcd0ee0f32c655148e5a640df009d8916ebdce
GTatjana13 <ptatjana13@gmail.com>
2023-07-09 00:19:14 +0300

    issue-delete is added

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -5,35 +5,33 @@ import IssueModal from "../pages/IssueModal.js";
     beforeEach(() => {
         cy.visit('/');
         cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-        //System will already open issue modal in beforeEach block  
-        cy.visit(url + '/board');
-        cy.contains('This is an issue of type: Task.').click();
+            //System will already open issue modal in beforeEach block  
+            cy.visit(url + '/board');
         });
-      });
+        cy.contains('This is an issue of type: Task.').click();
+
+        //Assert that detail modal is visible
+        IssueModal.getIssueDetailModal();
+    });
 
 
     it('Delete the issue successfully', () => {
-
-        IssueModal.clickDeleteButton()
-        IssueModal.confirmDeletion()
+        IssueModal.clickDeleteButton();
+        IssueModal.confirmDeletion();
 
         //Assert the pop-window disappeared
         cy.get(modalConfirm).should('not.exist');
 
         //Assert the issue is deleted
-        IssueModal.ensureIssueIsNotVisibleOnBoard('This is an issue of type: Task.')
-
+        IssueModal.ensureIssueIsNotVisibleOnBoard('This is an issue of type: Task.');
     });
 
     
     it('Starting the deleting issue process, but cancelling this action', () => {
-       
-        IssueModal.getIssueDetailModal() 
-        IssueModal.clickDeleteButton()
-        IssueModal.cancelDeletion()
-        IssueModal.closeDetailModal()
-        IssueModal.ensureIssueIsVisibleOnBoard('This is an issue of type: Task.')
-        
+        IssueModal.clickDeleteButton();
+        IssueModal.cancelDeletion();
+        IssueModal.closeDetailModal();
+        IssueModal.ensureIssueIsVisibleOnBoard('This is an issue of type: Task.'); 
     });
 
         
